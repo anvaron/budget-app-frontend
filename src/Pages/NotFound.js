6d2ef1f70fbd7b32c3b72cbeee51e7be72564d19
Andres Varon <anvaron@gmail.com>
2022-07-28 13:29:47 -0400

    Project update

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -6,9 +6,11 @@ export default function NotFound() {
     <section className="not__found">
       <h1>404 Error</h1>
       <h2>Page not found</h2>
+      <div className="form__control">
       <Link to={`/transactions`}>
         <button>Back to Transactions</button>
       </Link>
+      </div>
     </section>
   );
 }
